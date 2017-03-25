71d0d5262c02baa7939e489b045831834258ea5b
Rajendra arora <rajendraarora161992@gmail.com>
2017-03-25 18:49:10 +0530

    Adding new web application built in node.js usign express framework

---
+++ public/music/myplaylist.js
@@ -0,0 +1,185 @@
+var myPlaylist = [
+	
+	{
+		mp3:'music/files/what_is_love.mp3',
+		title:'What is love',
+		artist:'What is love',
+		rating:5,
+		duration:'4:00',
+		cover:'https://www.webdesign.org/img_articles/21649/What-Is-Love-Wallpaper-Final_550.jpg'
+	},
+	{
+		mp3:'http://us1.internet-radio.com:8105/;listen.pls',
+		title:'Classic Rock Radio 1',
+		artist:'Classic Rock Radio 1',
+		rating:1,
+		duration:'Radio',
+		cover:'https://yt3.ggpht.com/-rH_kU_Hze2M/AAAAAAAAAAI/AAAAAAAAAAA/Au9m-TYbVGw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg'
+	},
+    {
+        mp3:'http://uk5.internet-radio.com:8251/;listen.pls',
+        title:'Classic Rock Radio 2',
+        artist:'Classic Rock Radio 2',
+        rating:3,
+        duration:'Radio',
+        cover:'http://skar.com/wp-content/uploads/2014/10/skar-advertising-classic-radio-mistakes.jpg'
+    },
+    {
+        mp3:'http://uk6.internet-radio.com:8192/;listen.pls',
+        title:'Real dance Radio',
+        artist:'Real dance Radio',
+        rating:3,
+        duration:'Radio',
+        cover:'https://pbs.twimg.com/profile_images/772850740012412930/dHI2JDZ1.jpg'
+    },
+    {
+        mp3:'http://server.hitzconnect.com:8563/;stream1',
+        title:'Hitz one',
+        artist:'Hitz one',
+        rating:4,
+        duration:'Radio',
+        cover:'http://www.538-hitzone.nl/wp-content/uploads/2016/01/hitzone-76.jpg'
+    },
+    {
+        mp3:'http://93.84.112.253:8010/MV128',
+        title:'Hits channel',
+        artist:'Hits channel',
+        rating:4,
+        duration:'Radio',
+        cover:'http://3.bp.blogspot.com/-tTRX85NSUUM/Tu_mm_vOqSI/AAAAAAAAEgQ/6nawzBpTwQY/s1600/1025hitchannel.jpg'
+    },
+    {
+        mp3:'http://158.69.227.214:8113/stream/;stream/1',
+        title:'Hang 10 rocks radio',
+        artist:'Hand 10 rocks radio',
+        rating:2,
+        duration:'Radio',
+        cover:'http://radiovolna.net/uploads/posts/2017-02/1487795325_unnamed.jpg'
+    },
+    {
+        mp3:'http://206.190.144.154:8006/stream/;stream/1',
+        title:'Radio max music',
+        artist:'Radio max music',
+        rating:1,
+        duration:'Radio',
+        cover:'http://www.liveonlineradio.net/wp-content/uploads/2015/08/Radio-Max-Music.jpg'
+    },
+    {
+        mp3:'http://38.107.242.210:8012/;stream/1',
+        title:'Afrik best radio',
+        artist:'Afrik best radio',
+        rating:5,
+        duration:'Radio',
+        cover:'https://lh3.googleusercontent.com/v1YxVzM3iAhx0kgASKaPxLI9rsxwT8C372WxNMfNXFruhuUPNlkCqCXjNHrX1iO3yV-9=w300'
+    },
+    {
+        mp3:'http://whtz-fm.akacast.iheart.com/7/579/19972/v1/auth.akacast.akamaistream.net/whtz-fm',
+        title:'Z100 New york',
+        artist:'Z100 New york',
+        rating:5,
+        duration:'Radio',
+        cover:'http://static.radio.net/images/broadcasts/bc/61/41066/1/c175.png'
+    },
+    {
+        mp3:'http://kiis-fm.akacast.akamaistream.net/7/572/19773/v1/auth.akacast.akamaistream.net/kiis-fm?1490441753199',
+        title:'KIIS FM 102.7 FM',
+        artist:'Z100 New york',
+        rating:5,
+        duration:'Radio',
+        cover:'http://www.liveonlineradio.net/wp-content/uploads/2012/01/KIIS-FM2.jpg'
+    },
+    {
+        mp3:'http://wksc-fm.akacast.akamaistream.net/7/140/19891/v1/auth.akacast.akamaistream.net/wksc-fm',
+        title:'103.5 Kiss FM',
+        artist:'103.5 Kiss FM',
+        rating:4,
+        duration:'Radio',
+        cover:'https://pbs.twimg.com/profile_images/796225955006009345/YFiiDrs7.jpg'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/chr2/chainsmokersthe_ha201704_03_paris.m4a',
+        title:'The Chainsmoker - Paris',
+        artist:'The Chainsmoker - Paris',
+        rating:4,
+        duration:'3:37',
+        cover:'https://i.ytimg.com/vi/RhU9MZ98jxo/maxresdefault.jpg'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/chr2/legendjohn_ha201340_01_allofme.m4a',
+        title:'All of Me',
+        artist:'John legend',
+        rating:5,
+        duration:'4:12',
+        cover:'http://az616578.vo.msecnd.net/files/2016/08/08/636062251109136571516319197_John-Legend-All-Of-Me2.jpg'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/decades/henleydon_buildingtheperfec_09donhenley.m4a',
+        title:'The boys of summer',
+        artist:'Don henley',
+        rating:5,
+        duration:'4:44',
+        cover:'http://az616578.vo.msecnd.net/files/2016/08/08/636062251109136571516319197_John-Legend-All-Of-Me2.jpg'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/chr2/sheeraned_ha201703_3_shapeofyou.m4a',
+        title:'Shape of you',
+        artist:'Ed sheeran',
+        rating:5,
+        duration:'3:51',
+        cover:'http://i.axs.com/2017/01/22736-image_587e8cdbc64b9.jpg'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/chr2/malikzayn_idontwannaliveforeversingle_01_idontwannaliveforever.m4a',
+        title:'I dont wanna live forever',
+        artist:'Zayn and Taylor swift',
+        rating:5,
+        duration:'4:04',
+        cover:'https://i.ytimg.com/vi/7F37r50VUTQ/hqdefault.jpg'
+    },
+	{
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/chrgold/aceofbase_ch038_thesign.m4a',
+        title:'The sign',
+        artist:'Ace of base',
+        rating:1,
+        duration:'3:07',
+        cover:'https://i.scdn.co/image/40a223f0b82574100d833ffb5e0e1bcc23dc0705'
+    },
+    {
+        mp3:'http://d1qg6pckcqcdk0.cloudfront.net/numeric/102000/102923.m4a',
+        title:'Another night',
+        artist:'Real MacCoy',
+        rating:5,
+        duration:'3:07',
+        cover:'https://upload.wikimedia.org/wikipedia/en/0/06/Another_Night_%28album%29.jpg'
+    },
+    {
+        mp3:'http://store.naitimp3.ru/listen/2258848/d2hSanVLVXJTREFXZU9GZlA4UEpXbktWcHVFV0Q0SzAxd1hGWWhnNjM1VFVuRkNCMWVzcmpDQ1o4OFZ2QmdjOG9ZWnlFZlI4K1hZays0STRhdXp5MlNwVmw4VHljdDdncHVUT3RHdFIzQlk9/ace_of_base_ace_of_base_beautiful_life_(NaitiMP3.ru).mp3',
+        title:'Beautiful Life',
+        artist:'Ace of base',
+        rating:5,
+        duration:'3:42',
+        cover:'https://i.scdn.co/image/40a223f0b82574100d833ffb5e0e1bcc23dc0705'
+    }
+
+
+];
+jQuery(document).ready(function ($) {
+	$('.music-player-list').ttwMusicPlayer(myPlaylist, {
+		currencySymbol:'$',
+		buyText:'BUY',
+		tracksToShow:3,
+		autoplay:true,
+		ratingCallback:function(index, playlistItem, rating){
+			//some logic to process the rating, perhaps through an ajax call
+		},
+		jPlayer:{
+			swfPath: "js/Jplayer.swf",
+			supplied: "mp3",
+			volume:  0.8,
+			wmode:"window",
+			solution: "html,flash",
+			errorAlerts: true,
+			warningAlerts: false
+		}
+	});
+});
